2de127ede854d34e6fcf8f1f3cca8c0f0ccc4019
ArindanAditiya <indangaming003@gmail.com>
2025-02-09 09:34:15 +0700

    DAY 6 BELAJAR LARAVEL 11

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,5 +1,15 @@
 <x-layout>
   <x-slot:title>{{ $title }}</x-slot:title>
   <x-slot:header>{{ $header }}</x-slot:header>
-  <h4>Wow, Ada yang mau tahu Blog Aku</h4>
+  
+  <article class="container mb-2">
+    <h1>Article Title</h1>
+    <p><a href="#">Arindan Aditiya</a> | <span>16 Desember 2005</span></p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit dolor vitae porro, reiciendis vel ratione expedita rerum distinctio nisi delectus. Doloribus, incidunt nisi! Qui officia, rerum amet ducimus nisi, veritatis iste nihil, illo dolores perferendis quidem tempore quaerat? Quisquam pariatur laboriosam maiores dolore consequuntur dicta iure qui libero enim sapiente eveniet veritatis ipsum minima odit ratione sit voluptatum, quod voluptas praesentium. Pariatur, nobis molestiae nulla aliquid dolorem corporis voluptates alias quibusdam rem, quisquam impedit? Reiciendis molestias distinctio placeat, nobis eius illo asperiores repellendus nam, deleniti nesciunt tenetur! Eius ab labore architecto consequuntur mollitia, at voluptatibus magni sit cum, itaque unde.
+      <br>
+      <a href="#">Read More &raquo;</a>
+    </p>
+  </article>
+
 </x-layout>
